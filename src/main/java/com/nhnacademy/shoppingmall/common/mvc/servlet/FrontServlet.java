0c64d9d36f75a2d5745537c175261a44dea61438
m00n <mjm3204@naver.com>
2026-03-09 13:51:28 +0900

    FrontServlet

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
@@ -13,6 +13,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 @Slf4j
 @WebServlet(name = "frontServlet",urlPatterns = {"*.do"})
 public class FrontServlet extends HttpServlet {
@@ -22,17 +24,17 @@ public class FrontServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         //todo#7-1 controllerFactory를 초기화 합니다.
-
+        controllerFactory = (ControllerFactory) getServletContext().getAttribute(ControllerFactory.CONTEXT_CONTROLLER_FACTORY_NAME);
 
         //todo#7-2 viewResolver를 초기화 합니다.
-
+        viewResolver = new ViewResolver();
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp){
         try{
             //todo#7-3 Connection pool로 부터 connection 할당 받습니다. connection은 Thread 내에서 공유됩니다.
-
+            DbConnectionThreadLocal.initialize();
 
             BaseController baseController = (BaseController) controllerFactory.getController(req);
             String viewName = baseController.execute(req,resp);
@@ -41,7 +43,7 @@ public class FrontServlet extends HttpServlet {
                 String redirectUrl = viewResolver.getRedirectUrl(viewName);
                 log.debug("redirectUrl:{}",redirectUrl);
                 //todo#7-6 redirect: 로 시작하면  해당 url로 redirect 합니다.
-
+                resp.sendRedirect(redirectUrl);
             }else {
                 String layout = viewResolver.getLayOut(viewName);
                 log.debug("viewName:{}", viewResolver.getPath(viewName));
@@ -50,14 +52,18 @@ public class FrontServlet extends HttpServlet {
                 rd.include(req, resp);
             }
         }catch (Exception e){
-            log.error("error:{}",e);
+            log.error("error:{}",e.getMessage(),e);
             DbConnectionThreadLocal.setSqlError(true);
             //todo#7-5 예외가 발생하면 해당 예외에 대해서 적절한 처리를 합니다.
-
+            req.setAttribute("예외", e);
+            try {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }finally {
             //todo#7-4 connection을 반납합니다.
-
+            DbConnectionThreadLocal.reset();
         }
     }
-
 }
