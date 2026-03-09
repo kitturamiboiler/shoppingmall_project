7ed1b0933157bdb00305eda642a85f75cd394d50
m00n <mjm3204@naver.com>
2026-03-09 14:57:51 +0900

    complete: ApplicationListener

--- src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
@@ -1,12 +1,20 @@
 package com.nhnacademy.shoppingmall.common.listener;
 
+import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
+import com.nhnacademy.shoppingmall.user.domain.User;
 import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
 import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.annotation.WebFilter;
+import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Slf4j
 public class ApplicationListener implements ServletContextListener {
@@ -14,6 +22,26 @@ public class ApplicationListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         //todo#12 application 시작시 테스트 계정인 admin,user 등록합니다. 만약 존재하면 등록하지 않습니다.
-
+        try {
+            DbConnectionThreadLocal.initialize();
+            if (Objects.isNull(userService.getUser("admin"))) {
+                User admin = new User("admin", "어드민", "12345", "19900101",
+                        User.Auth.ROLE_ADMIN, 1_000_000, LocalDateTime.now(), null);
+                userService.saveUser(admin);
+            }
+            if (Objects.isNull(userService.getUser("user"))) {
+                User admin = new User("user", "유저", "12345", "19900102",
+                        User.Auth.ROLE_USER, 1_000_000, LocalDateTime.now(), null);
+                userService.saveUser(admin);
+            }
+        } catch (Exception e) {
+            DbConnectionThreadLocal.setSqlError(true);
+        } finally {
+            DbConnectionThreadLocal.reset();
+        }
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        log.info("운영 중단");
     }
 }
