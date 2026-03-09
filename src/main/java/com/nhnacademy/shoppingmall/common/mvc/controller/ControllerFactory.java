789ada1f4179e314d9a4c00c924769c220d40ffa
m00n <mjm3204@naver.com>
2026-03-09 13:06:30 +0900

    ViewResolver

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/controller/ControllerFactory.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/controller/ControllerFactory.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.shoppingmall.common.mvc.controller;
 
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.exception.ControllerNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,27 +36,48 @@ public class ControllerFactory {
          *  즉 /index.do, /main.do -> IndexController로 맵핑 됩니다.
          */
 
+        for (Class<?> clazz:c) {
+            RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
+            if (Objects.nonNull(requestMapping)) {
+                try {
+                    Object instance = clazz.getDeclaredConstructor().newInstance();
+                    String method = requestMapping.method().name();
+                    String[] paths = requestMapping.value();
 
+                    for (String path: paths) {
+                        String key = getKey(method,path);
+                        beanMap.put(key, instance);
+                    }
+                } catch (NoSuchMethodException | InstantiationException | IllegalArgumentException |
+                         InvocationTargetException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
         //#todo5-2 ctx(ServletContext)에  attribute를 추가합니다. -> key : CONTEXT_CONTROLLER_FACTORY_NAME, value : ControllerFactory
-
+        ctx.setAttribute(CONTEXT_CONTROLLER_FACTORY_NAME, this);
     }
 
     private Object getBean(String key){
         //todo#5-3 beanMap에서 controller 객체를 반환 합니다.
 
-        return null;
+        return beanMap.get(key);
     }
 
     public Object getController(HttpServletRequest request){
         //todo#5-4 request의 method, servletPath를 이용해서 Controller 객체를 반환합니다.
-
-        return null;
+        String method = request.getMethod();
+        String path = request.getServletPath();
+        return getController(method, path);
     }
 
     public Object getController(String method, String path){
         //todo#5-5 method, path를 이용해서 Controller 객체를 반환 합니다.
-
-        return null;
+        Object controller = getBean(getKey(method, path));
+        if (Objects.isNull(controller)) {
+            throw new ControllerNotFoundException(path);
+        }
+        return controller;
     }
 
     private String getKey(String method, String path){
@@ -60,6 +85,6 @@ public class ControllerFactory {
         //ex GET-/index.do
         //ex POST-/loginAction.do
 
-        return "";
+        return method + "-" + path;
     }
 }
--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/view/ViewResolver.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/view/ViewResolver.java
@@ -1,4 +1,8 @@
 package com.nhnacademy.shoppingmall.common.mvc.view;
+
+import java.util.Locale;
+import java.util.Objects;
+
 public class ViewResolver {
 
     public static final String DEFAULT_PREFIX="/WEB-INF/views/";
@@ -21,27 +25,34 @@ public class ViewResolver {
 
     public  String getPath(String viewName){
         //todo#6-1  prefix+viewNAme+postfix 반환 합니다.
-        return "";
+        if (viewName.startsWith("/")){
+            viewName = viewName.substring(1);
+        }
+        return prefix + viewName + postfix;
     }
 
-    public boolean isRedirect(String viewName){
+    public boolean isRedirect(String viewName) {
         //todo#6-2 REDIRECT_PREFIX가 포함되어 있는지 체크 합니다.
-        return false;
+        if (Objects.isNull(viewName)) return false; {
+            return viewName.toLowerCase().startsWith(REDIRECT_PREFIX.toLowerCase());
+        }
     }
-
     public String getRedirectUrl(String viewName){
         //todo#6-3 REDIRECT_PREFIX를 제외한 url을 반환 합니다.
-
-        return "";
+        if (isRedirect(viewName)) {
+            return viewName.substring(REDIRECT_PREFIX.length());
+        }
+        return viewName;
     }
-
     public String getLayOut(String viewName){
 
         /*todo#6-4 viewName에
            /admin/경로가 포함되었다면 DEFAULT_ADMIN_LAYOUT 반환 합니다.
            /admin/경로가 포함되어 있지않다면 DEFAULT_SHOP_LAYOUT 반환 합니다.
         */
-
+        if (Objects.nonNull(viewName) && (viewName.startsWith("/admin") || viewName.contains("/admin/"))) {
+            return DEFAULT_ADMIN_LAYOUT;
+        }
         return DEFAULT_SHOP_LAYOUT;
     }
 }
