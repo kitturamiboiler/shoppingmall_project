bef0dc8354dd50119036925fa881db0577f26e22
m00n <mjm3204@naver.com>
2026-03-09 14:19:29 +0900

    Complete: LoginCheckFilter, WelcomePageFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
@@ -17,6 +17,10 @@ public class WelcomePageFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#9 /요청이 오면 welcome page인 index.do redirect 합니다.
-
+        if (req.getServletPath().equals("/")) {
+            res.sendRedirect("/index.do");
+            return;
+        }
+        chain.doFilter(req,res);
     }
 }
