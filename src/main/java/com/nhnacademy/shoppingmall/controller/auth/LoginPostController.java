39fe14c216cfcb298494ac04ec28bcd9247e8d56
m00n <mjm3204@naver.com>
2026-03-09 16:18:48 +0900

    complete: loginController

--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
@@ -2,12 +2,14 @@ package com.nhnacademy.shoppingmall.controller.auth;
 
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.user.domain.User;
 import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
 import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @RequestMapping(method = RequestMapping.Method.POST,value = "/loginAction.do")
 public class LoginPostController implements BaseController {
@@ -17,7 +19,17 @@ public class LoginPostController implements BaseController {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
         //todo#13-2 로그인 구현, session은 60분동안 유지됩니다.
-
-        return "shop/main/index";
+        String userId = req.getParameter("user_id");
+        String userPw = req.getParameter("user_password");
+        try {
+            User user = userService.doLogin(userId, userPw);
+            HttpSession httpSession = req.getSession(true);
+            httpSession.setAttribute("user", user);
+            httpSession.setMaxInactiveInterval(3600);
+            return "redirect:/index.do";
+        } catch (Exception e) {
+            req.setAttribute("error", "아이디 비번 잘못 입력");
+            return "shop/main/index";
+        }
     }
 }
