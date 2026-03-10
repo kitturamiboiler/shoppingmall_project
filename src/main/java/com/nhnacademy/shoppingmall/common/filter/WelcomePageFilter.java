05745c6e47a83016d135dc579bf7a8689c9ca3db
{gogooga1} <{dudrn322056@naver.com}>
2026-03-10 11:35:03 +0900

    welcomePage 수정

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
@@ -12,15 +12,14 @@ import java.io.IOException;
 
 @Slf4j
 
+@WebFilter(value = "/")
 public class WelcomePageFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#9 /요청이 오면 welcome page인 index.do redirect 합니다.
-        if (req.getServletPath().equals("/")) {
-            res.sendRedirect("/index.do");
-            return;
-        }
-        chain.doFilter(req,res);
+
+        res.sendRedirect("/index.do");
+
     }
 }
