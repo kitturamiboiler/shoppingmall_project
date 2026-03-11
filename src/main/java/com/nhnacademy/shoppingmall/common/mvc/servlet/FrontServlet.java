77a670d34f76dd45870be3f2c6eb1c22da5a8636
m00n <mjm3204@naver.com>
2026-03-11 13:22:54 +0900

    welcomePageFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 
 @Slf4j
